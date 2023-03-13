6f73e940f06aeecb18185713a0f9d9a9f56ddf69
Evangelos Baras <baggelisbaras@hotmail.com>
2023-03-13 18:03:50 +0200

    Add files via upload

--- Tests/SCPI/SCPI.ino
+++ Tests/SCPI/SCPI.ino
@@ -26,46 +26,10 @@ void setup() {
 	my_instrument.RegisterCommand(F("TEMPerature?"), &GetTemperature);	
     my_instrument.RegisterCommand(F("ERRor?"), &GetLastEror);
 	//Reset the command tree base to the root level
-	my_instrument.SetCommandTreeBase("");
+	//my_instrument.SetCommandTreeBase("");
     my_instrument.SetErrorHandler(&myErrorHandler);
-    /*
-	my_instrument.SetCommandTreeBase("MEASure");
-    my_instrument.RegisterCommand(":VOLTage?", &SetIP);
-    my_instrument.RegisterCommand(":CURRent?", &GetIP);
-    my_instrument.RegisterCommand(":POWEr?", &SetGW);
-    my_instrument.SetCommandTreeBase("SET:");
-    my_instrument.RegisterCommand(":VOLTage?", &SetIP);
-    my_instrument.RegisterCommand(":CURRent?", &GetIP);
-    my_instrument.RegisterCommand(":POWEr?", &SetGW);
-    ======
-	my_instrument.SetCommandTreeBase(F("SOURce"));
-		my_instrument.RegisterCommand(F(":VOLTage:VLIMt"), &SetVoltageLimit);
-		my_instrument.RegisterCommand(F(":VOLTage:VLIMt?"), &GetVoltageLimit);
-		my_instrument.RegisterCommand(F(":CURRent:ILIMt"), &SetCurrentLimit);
-		my_instrument.RegisterCommand(F(":CURRent:ILIMt?"), &GetCurrentLimit);
-		my_instrument.RegisterCommand(F(":RESistance:RLIMt"), &SetResistanceLimit);
-		my_instrument.RegisterCommand(F(":RESistance:RLIMt?"), &GetResistanceLimit);
-		my_instrument.RegisterCommand(F(":POWer:PLIMt"), &SetPowerLimit);
-		my_instrument.RegisterCommand(F(":POWer:PLIMt?"), &GetPowerLimit);
-		my_instrument.RegisterCommand(F(":INPut:STATe"), &SetInputState);
-		my_instrument.RegisterCommand(F(":INPut:STATe?"), &GetInputState);
-		my_instrument.RegisterCommand(F(":LIST:MODE"), &SetListMode);
-		my_instrument.RegisterCommand(F(":LIST:MODE?"), &GetListMode);
-	my_instrument.SetCommandTreeBase("");
-	
-	my_instrument.SetCommandTreeBase(F("MEASure"));
-		my_instrument.RegisterCommand(F(":VOLTage:DC?"), &GetVoltageDC);
-		my_instrument.RegisterCommand(F(":CURRent:DC?"), &GetCurrentDC);
-		my_instrument.RegisterCommand(F(":POWer:DC?"), &GetPowerDC);
-		my_instrument.RegisterCommand(F(":RESistance:DC?"), &GetResistanceDC);
-						
-		my_instrument.RegisterCommand(F(":CAPability?"), &GetCapability);
-		my_instrument.RegisterCommand(F(":WATThours?"), &GetWattHours);
-		my_instrument.RegisterCommand(F(":DISChargingTime?"), &GetDischargingTime);
-	my_instrument.SetCommandTreeBase("");
-	*/
-	
-	// Set up the parent command node for source-related commands
+    
+	// Set up the parent command node for setPoint commands
 	my_instrument.SetCommandTreeBase(F("SOURce"));
 		// Register commands for setting and requesting voltage, current, power, and resistance
 		my_instrument.RegisterCommand(F("VOLTage:DC"), &SetCVpoint);		
@@ -94,26 +58,18 @@ void setup() {
 		
 		my_instrument.RegisterCommand(F("CURRent:ILIMt?"), &GetCurrentLimit);
 		my_instrument.RegisterCommand(F("CURRent:ILIMt"), &SetCurrentLimit);
-	//Reset the command tree base to the root level
-	my_instrument.SetCommandTreeBase("");
-
-     //Set up the parent command node for ADC measurement commands
+	//Set up the parent command node for ADC measurement commands
     my_instrument.SetCommandTreeBase(F("MEASure"));
 		//Register commands for requesting voltage, current, resistance, and power measurements
-		my_instrument.RegisterCommand(F("VOLTage:DC?"), &GetVoltage);
-		my_instrument.RegisterCommand(F("CURRent:DC?"), &GetCurrent);
-		//my_instrument.RegisterCommand(F("RESistance:DC?"), &GetResistance);//Crashes the arduino when i enable this
-		//my_instrument.RegisterCommand(F("POWer:DC?"), &GetPower);//arduino not replying to anything when i open this 
-
-		//(Communication timeout error)
-
-	//my_instrument.RegisterCommand(F(":CAPability?"), &GetAhours);
-	//my_instrument.RegisterCommand(F(":WATThours?"), &GetWattHours);
-
-	//	//Register command for requesting discharge time measurement
-	//	my_instrument.RegisterCommand(F("DISChargingTime?"), &GetDischargeTime);
-	
- //    //Reset the command tree base to the root level
+		my_instrument.RegisterCommand(F("VOLTage:DC?"), &GetVoltage);//Crashes the arduino when i enable this
+		my_instrument.RegisterCommand(F("CURRent:DC?"), &GetCurrent);//arduino not replying to anything when i open this 
+		my_instrument.RegisterCommand(F("RESistance:DC?"), &GetResistance);//Did we reach the limit of the Library in terms of command set? 
+		my_instrument.RegisterCommand(F("POWer:DC?"), &GetPower);//im either getting (Communication timeout error) or a totaly crashed avr			
+		my_instrument.RegisterCommand(F("CAPability?"), &GetAhours);
+		my_instrument.RegisterCommand(F("WATThours?"), &GetWattHours);
+		//Register command for requesting discharge time measurement
+		my_instrument.RegisterCommand(F("DISChargingTime?"), &GetDischargeTime); 
+		//Reset the command tree base to the root level
     my_instrument.SetCommandTreeBase("");
 
 	
@@ -142,8 +98,12 @@ Ethernet.init(csPin); // initialize the Ethernet library
 }
 
 void loop() {
-	fromSerial = true;
-	my_instrument.ProcessInput(Serial, "\n");
+	if (fromSerial = true) {
+		while (Serial.available())
+		{
+			my_instrument.ProcessInput(Serial, "\n");
+		}
+	}
 
 	client = server.available();
 	if (client.connected()) {
@@ -187,35 +147,93 @@ void SetCVpoint(SCPI_C commands, SCPI_P parameters, Stream& interface) {
 }
 
 void GetCCpoint(SCPI_C commands, SCPI_P parameters, Stream& interface) {
-
+	float f = 1.02;
+	interface.println(f, 3);
 }
 
 void SetCCpoint(SCPI_C commands, SCPI_P parameters, Stream& interface) {
-
+	float cc;
+	//// Here we print the recieved commands.
+	//	for (size_t i = 0; i < commands.Size(); i++) {
+	//		Serial.print(commands[i]);
+	//		if (i < commands.Size() - 1) {
+	//			Serial.print(":");
+	//		}
+	//		else {
+	//			Serial.print(" ");
+	//		}
+	//	}
+	Serial.println("Recived Params:");
+	//Here we print the recieved parameters.
+	for (size_t i = 0; i < parameters.Size(); i++) {
+		cc=atof( parameters[i]);
+		if (i < parameters.Size() - 1)
+			return;
+	}//why the fak im doing Float , strings and ptr in 2AM again ? 
+	Serial.print(cc, 3);
 }
 
 void GetCPpoint(SCPI_C commands, SCPI_P parameters, Stream& interface) {
-
+	float f = 6.02;
+	interface.println(f, 3);
 }
 
 void SetCPpoint(SCPI_C commands, SCPI_P parameters, Stream& interface) {
-
+	float cp;
+	
+	Serial.println("Recived Params:");
+	//Here we print the recieved parameters.
+	for (size_t i = 0; i < parameters.Size(); i++) {
+		cp = atof(parameters[i]);
+		if (i < parameters.Size() - 1)
+			return;
+	}
+	Serial.print(cp, 3);
 }
 
 void GetCRpoint(SCPI_C commands, SCPI_P parameters, Stream& interface) {
-
+	float f = 5.02;
+	interface.println(f, 3);
 }
 
 void SetCRpoint(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+	float cp;
 
+	Serial.println("Recived Params:");
+	//Here we print the recieved parameters.
+	for (size_t i = 0; i < parameters.Size(); i++) {
+		cp = atof(parameters[i]);
+		if (i < parameters.Size() - 1)
+			return;
+	}
+	Serial.print(cp, 3);
 }
 
 void SetLoadState(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+	String LoadState = String(parameters.First());
+	
 
+	Serial.print("Recived Params:");
+	//Here we print the recieved parameters.
+	//for (size_t i = 0; i < strlen(parameters[0]); i++) {
+		if (LoadState == "1") {
+			Serial.println("1");
+			//LoadState = true;
+		}
+		if (LoadState == "0") {
+			Serial.println("0");
+			//LoadState = false;
+		}
+		//Serial.println(parameters[i]);
+		//if (i < parameters.Size() - 1)
+			//return;
+	//}
+	//Serial.println(LoadState);
 }
 
 void GetLoadState(SCPI_C commands, SCPI_P parameters, Stream& interface) {
-
+	float f = 7.02;
+	interface.println(f, 3);
 }
 
 void SetWorkingMode(SCPI_C commands, SCPI_P parameters, Stream& interface) {
@@ -223,7 +241,8 @@ void SetWorkingMode(SCPI_C commands, SCPI_P parameters, Stream& interface) {
 }
 
 void GetWorkingMode(SCPI_C commands, SCPI_P parameters, Stream& interface) {
-
+	
+	interface.println(1);
 }
 
 void SetVoltageLimit(SCPI_C commands, SCPI_P parameters, Stream& interface) {
@@ -231,7 +250,8 @@ void SetVoltageLimit(SCPI_C commands, SCPI_P parameters, Stream& interface) {
 }
 
 void GetVoltageLimit(SCPI_C commands, SCPI_P parameters, Stream& interface) {
-
+	float f = 46.09;
+	interface.println(f, 3);
 }
 
 void SetCurrentLimit(SCPI_C commands, SCPI_P parameters, Stream& interface) {
@@ -239,26 +259,45 @@ void SetCurrentLimit(SCPI_C commands, SCPI_P parameters, Stream& interface) {
 }
 
 void GetCurrentLimit(SCPI_C commands, SCPI_P parameters, Stream& interface) {
-
+	float f = 45;
+	interface.println(f, 3);
 }
 
 void GetVoltage(SCPI_C commands, SCPI_P parameters, Stream& interface) {
-
+	float f = 42.902;
+	interface.println(f, 3);
 }
 void GetCurrent(SCPI_C commands, SCPI_P parameters, Stream& interface) {
-
+	float f = 8.02;
+	interface.println(f, 3);
 }
 void GetPower(SCPI_C commands, SCPI_P parameters, Stream& interface) {
-
+	float f = 9.02;
+	interface.println(f, 3);
 }
 void GetResistance(SCPI_C commands, SCPI_P parameters, Stream& interface) {
-
+	float f = 100.1;
+	interface.println(f, 3);
 }
 
 void GetTemperature(SCPI_C commands, SCPI_P parameters, Stream& interface) {
-
+	float f = 32;
+	interface.println(f, 1);
 }
 
+void GetAhours(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+	float f =4900;
+	interface.println(f, 3);
+}
+void GetWattHours(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+	float f = 5100;
+	interface.println(f, 3);
+}
+
+void GetDischargeTime(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+	float f = 6969;
+	interface.println(f, 1);
+}
 void GetLastEror(SCPI_C commands, SCPI_P parameters, Stream& interface) {
 	switch (my_instrument.last_error) {
 	case my_instrument.ErrorCode::BufferOverflow:
